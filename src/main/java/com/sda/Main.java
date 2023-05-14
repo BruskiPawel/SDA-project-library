d2aa07ec64b61318da977c59ebcd1bcd6551b486
Penknife92 <112814704+Penknife92@users.noreply.github.com>
2023-05-14 14:32:42 +0200

    Changes by wojtek

--- src/main/java/com/sda/Main.java
+++ src/main/java/com/sda/Main.java
@@ -8,8 +8,14 @@ import com.sda.view.View;
 public class Main {
 
     public static void main(String[] args) {
+        View booksView = new BooksByAuthorView();
+        booksView.display();
         View view = new LoginView();
         view.display();
+
     }
+
+    // TODO wyświetlanie opcji po kolei niezależnie od roli, uniwersalnie nie tylko dla menu,
+    //  ale też ksiązek i użytkowników (typ generyczny)
 
 }
